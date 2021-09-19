13c688a0dedd3f2549e94a193c20542a0cfbfb17
ZF-1000 <n.shanin@mail.ru>
2021-09-19 21:38:52 +0300

    387. First Unique Character in a String

---
+++ 387. First Unique Character in a String.py
@@ -0,0 +1,49 @@
+"""
+Given a string s, find the first non-repeating character in it and return its index.
+If it does not exist, return -1.
+
+
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+"""
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        j = 1
+        for i in range(len(s)):
+            if len(s) == 1:
+                return 0
+            while j < len(s):
+                if i != j and s[i] == s[j]:
+                    j = 0
+                    break
+                if j == len(s) - 1:
+                    return i
+                j += 1
+        return -1
+
+
+# arr = ["aabb"]
+arr = ["leetcode", "loveleetcode", "aabb", "z", "acaadcad"]
+
+sol = Solution()
+for el in arr:
+    print(sol.firstUniqChar(el))
